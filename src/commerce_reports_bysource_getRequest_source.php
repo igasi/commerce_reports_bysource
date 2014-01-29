516029ab1efa65f7c24149dcfeb643d27d8803c1
Isaac García <isaac.gasi@gmail.com>
2014-01-29 12:31:29 -0600

    Refactoring composer and http-foundation request

--- src/commerce_reports_bysource_getRequest_source.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . './vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-
-$request = Request::createFromGlobals();
-
-$src = $request->query->get('src');
